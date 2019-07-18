9a6d525cb4ec25a16d81e635899bb0a8faf29d7b
MeTo <m_popstoilov@abv.bg>
2019-07-18 17:11:28 +0300

    Add ProductTypesController, create product type view

---
+++ Metomarket/Web/Metomarket.Web.ViewModels/ProductTypes/ProductTypeCreateInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Metomarket.Web.ViewModels.ProductTypes
+{
+    public class ProductTypeCreateInputModel
+    {
+        private const int NameMinLength = 2;
+        private const int NameMaxLength = 20;
+
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+        public string Name { get; set; }
+    }
+}
---
+++ Metomarket/Web/Metomarket.Web/Areas/Market/Controllers/ProductTypesController.cs
@@ -0,0 +1,33 @@
+﻿using Metomarket.Common;
+using Metomarket.Web.ViewModels.ProductTypes;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Metomarket.Web.Areas.Market.Controllers
+{
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    public class ProductTypesController : MarketController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Create()
+        {
+            return this.View(new ProductTypeCreateInputModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductTypeCreateInputModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
+    }
+}
