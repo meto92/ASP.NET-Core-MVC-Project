91f97cd42e061a24139fe8ce28a75283cf49db10
MeTo <m_popstoilov@abv.bg>
2019-08-11 15:07:19 +0300

    Add dashboard controller and users controller tests

---
+++ Metomarket/Tests/Metomarket.Web.Tests/Controlers/DashboardControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Metomarket.Common;
+using Metomarket.Data.Models;
+using Metomarket.Web.Areas.Administration.Controllers;
+using Metomarket.Web.ViewModels.Dashboard;
+
+using MyTested.AspNetCore.Mvc;
+
+using Xunit;
+
+namespace Metomarket.Web.Tests.Controlers
+{
+    public class DashboardControllerTests
+    {
+        [Fact]
+        public void DashboardControllerShouldInheritAdministrationControllerWhichShouldBeOnlyForAdmins()
+        {
+            Assert.True(typeof(AdministrationController).IsAssignableFrom(typeof(DashboardController)));
+
+            MyMvc
+                .Controller<AdministrationController>()
+                .ShouldHave()
+                .Attributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests(GlobalConstants.AdministratorRoleName));
+        }
+
+        [Fact]
+        public void IndexShouldReturnViewWithEntityCounts()
+            => MyMvc
+                .Controller<DashboardController>()
+                .WithData(db => db
+                    .WithSet<Contract>(c => c.Add(new Contract()))
+                    .WithSet<Order>(o => o.AddRange(this.CreateEntities<Order>(2)))
+                    .WithSet<Product>(p => p.AddRange(this.CreateEntities<Product>(3)))
+                    .WithSet<ProductType>(pt => pt.AddRange(this.CreateEntities<ProductType>(2)))
+                    .WithSet<ApplicationUser>(u => u.Add(new ApplicationUser())))
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<DashboardIndexViewModel>()
+                    .Passing(model =>
+                    {
+                        Assert.Equal(1, model.ContractsCount);
+                        Assert.Equal(2, model.OrdersCount);
+                        Assert.Equal(3, model.ProductsCount);
+                        Assert.Equal(2, model.ProductTypesCount);
+                        Assert.Equal(1, model.UsersCount);
+                    }));
+
+        private IEnumerable<TEntity> CreateEntities<TEntity>(int count)
+            where TEntity : class, new()
+            => Enumerable.Range(1, count)
+                .Select(n => new TEntity());
+    }
+}
---
+++ Metomarket/Tests/Metomarket.Web.Tests/Controlers/UsersControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using Metomarket.Common;
+using Metomarket.Data.Models;
+using Metomarket.Web.Areas.Administration.Controllers;
+
+using MyTested.AspNetCore.Mvc;
+
+using Xunit;
+
+namespace Metomarket.Web.Tests.Controlers
+{
+    public class UsersControllerTests
+    {
+        private const string User1Id = nameof(User1Id);
+        private const string User2Id = nameof(User2Id);
+        private const string AdminRoleId = nameof(AdminRoleId);
+        private const string AdminRoleName = GlobalConstants.AdministratorRoleName;
+
+        [Fact]
+        public void UsersControllerShouldInheritAdministrationControllerWhichShouldBeOnlyForAdmins()
+        {
+            Assert.True(typeof(AdministrationController).IsAssignableFrom(typeof(UsersController)));
+
+            MyMvc
+                .Controller<AdministrationController>()
+                .ShouldHave()
+                .Attributes(attrs => attrs
+                    .RestrictingForAuthorizedRequests(AdminRoleName));
+        }
+
+        [Fact]
+        public void IndexShouldReturnView()
+            => MyMvc
+                .Controller<UsersController>()
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void PromoteShouldRedirect()
+            => MyMvc
+                .Controller<UsersController>()
+                .WithData(db => db
+                    .WithSet<ApplicationRole>(set => set
+                        .Add(this.CreateAdminRole()))
+                    .WithSet<ApplicationUser>(set => set
+                        .Add(new ApplicationUser { Id = User1Id })))
+                .Calling(c => c.Promote(User1Id))
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .ToAction(nameof(UsersController.Index)));
+
+        [Fact]
+        public void DemoteShouldRedirect()
+            => MyMvc
+                .Controller<UsersController>()
+                .WithData(db => db
+                    .WithSet<ApplicationRole>(set => set
+                        .Add(this.CreateAdminRole()))
+                    .WithSet<ApplicationUser>(set => set
+                        .Add(new ApplicationUser { Id = User2Id })))
+                .Calling(c => c.Demote(User2Id))
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .ToAction(nameof(UsersController.Index)));
+
+        private ApplicationRole CreateAdminRole()
+        {
+            ApplicationRole adminRole = new ApplicationRole
+            {
+                Id = AdminRoleId,
+                Name = AdminRoleName,
+            };
+
+            return adminRole;
+        }
+    }
+}
